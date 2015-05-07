fc626022cefdb0ac3d0eb736bc495df60a3e2c46
Sergio Pavez <lestat.baeza@gmail.com>
2015-05-06 21:55:27 -0300

    Fragment para Login
    La aplicación inicia y carga el fragment del login. El botón para crear
    cuenta no cambia al frament para ello, se debe reparar.

---
+++ Hermes/MainActivity.cs
@@ -0,0 +1,48 @@
+﻿using Android.App;
+using Android.Views;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Widget;
+using Toolbar = Android.Support.V7.Widget.Toolbar;
+
+namespace Hermes
+{
+	[Activity (Label = "Hermes", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : AppCompatActivity
+	{
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+			var toolbar = FindViewById<Toolbar> (Resource.Id.toolbar);
+
+			//Toolbar will now take on default actionbar characteristics
+			SetSupportActionBar (toolbar);
+
+			SupportActionBar.Title = "Hermes";
+
+			replaceFragment(new LoginFragment ());
+
+        }
+
+		public void replaceFragment(Fragment f){
+			Toast.MakeText (this, "Cambiar Fragment", ToastLength.Long).Show();
+
+			// Create a new fragment and a transaction.
+			FragmentTransaction fragmentTx = this.FragmentManager.BeginTransaction();
+			// The fragment will have the ID of Resource.Id.fragment_container.
+			fragmentTx.Add(Resource.Id.fragment_container, f);
+			// Add the transaction to the back stack.
+			fragmentTx.AddToBackStack(null);
+			// Commit the transaction.
+			fragmentTx.Commit();
+
+		}
+
+    }
+}
+
---
+++ Hermes/Resources/LoginFragment.cs
@@ -0,0 +1,61 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace Hermes
+{
+	public class LoginFragment : Fragment, View.IOnClickListener
+	{
+
+		private Button _signup, _login;
+
+		public override void OnCreate (Bundle savedInstanceState)
+		{
+			base.OnCreate (savedInstanceState);
+
+			// Create your fragment here
+
+		}
+
+		public override View OnCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+		{
+			var view = inflater.Inflate (Resource.Layout.layout_login, container, false);
+
+			_signup = view.FindViewById<Button>(Resource.Id.signup);
+			_signup.SetOnClickListener (this);
+			_login = view.FindViewById<Button>(Resource.Id.login);
+			_login.SetOnClickListener (this);
+
+			// Use this to return your custom view for this Fragment
+			// return inflater.Inflate(Resource.Layout.YourFragment, container, false);
+			return view;
+		}
+			
+		public void OnClick (View v)
+		{
+
+			switch (v.Id) 
+			{
+			case Resource.Id.signup:
+				((MainActivity) this.Activity).replaceFragment(new SignupFragment ());
+				break;
+			case Resource.Id.login:
+				break;
+			default:
+				break;
+			}
+
+		}
+	}
+}
+
