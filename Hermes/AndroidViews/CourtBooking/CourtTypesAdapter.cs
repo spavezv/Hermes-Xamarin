410233897729946d079e7772e8cbe2bb5e8d49f5
nuris <nreyesq@alumnos.utalca.cl>
2015-06-08 16:06:13 -0300

    img tipos de cancha

--- Hermes/AndroidViews/CourtBooking/CourtTypesAdapter.cs
+++ Hermes/AndroidViews/CourtBooking/CourtTypesAdapter.cs
@@ -10,6 +10,7 @@ namespace Hermes.AndroidViews.CourtBooking
 	{
 		public List<string> items;
 		public AppCompatActivity context;
+		public ImageView imgType;
 
 		public CourtTypesAdapter(AppCompatActivity c, List<string> items):base() 
 		{
@@ -38,15 +39,33 @@ namespace Hermes.AndroidViews.CourtBooking
 			View row = convertView;
 			if(row == null)
 			{
-				row = LayoutInflater.From (context).Inflate(Resource.Layout.item_listview, null, false);
+				row = LayoutInflater.From (context).Inflate(Resource.Layout.item_type_lstv, null, false);
 			}
-			TextView txtCourtType = row.FindViewById<TextView> (Resource.Id.txt_drawer_item);
+			TextView txtCourtType = row.FindViewById<TextView> (Resource.Id.txt_type_item);
+			imgType = row.FindViewById<ImageView> (Resource.Id.img_type_item);
+
 			txtCourtType.Text = items [position];
+			setImage (items[position]);
 
 			return row;
 		}
 
 
+		void setImage (string str)
+		{
+			if (str.Contains ("Futbol") || str.Contains ("futbol")) {
+				imgType.SetImageResource (Resource.Drawable.ic_futbol);
+			} 
+			if (str.Contains ("Tenis") || str.Contains ("tenis")) {
+				imgType.SetImageResource (Resource.Drawable.ic_tennis);
+			} 
+			if (str.Contains ("Basquetball") || str.Contains ("basquetball")) {
+				imgType.SetImageResource (Resource.Drawable.ic_basketball);
+			}
+			if (str.Contains ("Voleyball") || str.Contains ("voleyball")) {
+				imgType.SetImageResource (Resource.Drawable.ic_volleyball);
+			}
+		}
 	}
 }
 
