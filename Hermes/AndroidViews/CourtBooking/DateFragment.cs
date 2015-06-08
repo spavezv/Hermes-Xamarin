50904903a6ab059a47df3fe6c08eaa1ffc1f1393
Sergio Pavez <lestat.baeza@gmail.com>
2015-06-08 04:02:52 -0300

    Mejora en la selección de fecha
    Se parte con la fecha de hoy seleccionada.
    Se setea la fecha mínima del calendario (no se pueden seleccionar días
    pasados)

--- Hermes/AndroidViews/CourtBooking/DateFragment.cs
+++ Hermes/AndroidViews/CourtBooking/DateFragment.cs
@@ -11,81 +11,76 @@ using Java.Util;
 
 namespace Hermes.AndroidViews.CourtBooking
 {
-	public class DateFragment: Fragment , View.IOnClickListener
-	{
-		public ImageView imgLeft, imgRight;
-		private TextView txtCategory;
-		public DatePicker datePicker;
+    public class DateFragment : Fragment, View.IOnClickListener
+    {
+        public ImageView imgLeft, imgRight;
+        private TextView txtCategory;
+        public DatePicker datePicker;
 
-		public override void OnCreate(Bundle savedInstanceState)
-		{
-			base.OnCreate(savedInstanceState);
-		}
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+        }
 
-		public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
-		{
-			var view = inflater.Inflate(Resource.Layout.booking_date_fragment, container, false);
-			imgLeft = view.FindViewById<ImageView>(Resource.Id.img_arrow_left);
-			imgRight = view.FindViewById<ImageView>(Resource.Id.img_arrow_right);
-			txtCategory = view.FindViewById<TextView>(Resource.Id.txt_book_header);
-			datePicker = view.FindViewById<DatePicker> (Resource.Id.date_boking_fragment);
-			txtCategory.SetText (Resource.String.FechadeReserva);
-			imgRight.SetImageResource (Resource.Drawable.ic_arrow_right_disable);
-			imgLeft.SetOnClickListener (this);
-			imgRight.SetImageResource (Resource.Drawable.ic_arrow_right_available);
-			imgRight.SetOnClickListener (this);
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            var view = inflater.Inflate(Resource.Layout.booking_date_fragment, container, false);
+            imgLeft = view.FindViewById<ImageView>(Resource.Id.img_arrow_left);
+            imgRight = view.FindViewById<ImageView>(Resource.Id.img_arrow_right);
+            txtCategory = view.FindViewById<TextView>(Resource.Id.txt_book_header);
+            datePicker = view.FindViewById<DatePicker>(Resource.Id.date_boking_fragment);
+            txtCategory.SetText(Resource.String.FechadeReserva);
+            imgRight.SetImageResource(Resource.Drawable.ic_arrow_right_disable);
+            imgLeft.SetOnClickListener(this);
+            imgRight.SetImageResource(Resource.Drawable.ic_arrow_right_available);
+            imgRight.SetOnClickListener(this);
 
-			datePicker.MinDate = (long)(DateTime.Today.Millisecond);
-			datePicker.Init (
-				DateTime.Now.Year,
-				DateTime.Now.Month - 1,
-				DateTime.Now.Day,
-				new AndroidDatePickerListener (this));
-			return view;
-		}
+            datePicker.MinDate = (long)(DateTime.Today.Millisecond);
+            datePicker.Init(
+                DateTime.Now.Year,
+                DateTime.Now.Month - 1,
+                DateTime.Now.Day,
+                new AndroidDatePickerListener(this));
+            datePicker.FirstDayOfWeek = Java.Util.Calendar.Monday;
+            datePicker.UpdateDate(DateTime.Now.Year, DateTime.Now.Month - 1, DateTime.Now.Day);
+            datePicker.MinDate = new Java.Util.Date().Time - 1000;
+            return view;
+        }
 
-		public void OnClick(View v)
-		{
-			
-			switch (v.Id)
-			{
-			case Resource.Id.img_arrow_left:
-				((HermesActivity)this.Activity).replaceFragment(new TypeFragment());		
-				break;
-			case Resource.Id.img_arrow_right:
-				Console.WriteLine ();
-				((HermesActivity)this.Activity).replaceFragment(new BranchFragment());		
-				break;
-			default:
-				break;
-			}
+        public void OnClick(View v)
+        {
 
-		}
+            switch (v.Id)
+            {
+                case Resource.Id.img_arrow_left:
+                    ((HermesActivity)this.Activity).replaceFragment(new TypeFragment());
+                    break;
+                case Resource.Id.img_arrow_right:
+                    ((HermesActivity)this.Activity).replaceFragment(new BranchFragment());
+                    break;
+                default:
+                    break;
+            }
 
-
-		public class AndroidDatePickerListener: Java.Lang.Object, DatePicker.IOnDateChangedListener {
-			DateFragment mBookCourtDateFrag;
-			public AndroidDatePickerListener (DateFragment bookingCourtDateFragment)
-			{
-				mBookCourtDateFrag = bookingCourtDateFragment;
-			}
-			public void OnDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-				//capturar la fecha
-				var date = new DateTime(year, monthOfYear +1 , dayOfMonth);
-				((HermesActivity)mBookCourtDateFrag.Activity).DateEsp= date.ToString("d MMMM", 
-					CultureInfo.CreateSpecificCulture("es-MX"));
-        ((HermesActivity)mBookCourtDateFrag.Activity).Date= date.ToString("yyyy-MM-dd");
-			}
+        }
 
 
-		}
-
-		public void OnDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth)
-		{
-			
-
-
-		}
-	}
+        public class AndroidDatePickerListener : Java.Lang.Object, DatePicker.IOnDateChangedListener
+        {
+            DateFragment mBookCourtDateFrag;
+            public AndroidDatePickerListener(DateFragment bookingCourtDateFragment)
+            {
+                mBookCourtDateFrag = bookingCourtDateFragment;
+            }
+            public void OnDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth)
+            {
+                //capturar la fecha
+                var date = new DateTime(year, monthOfYear + 1, dayOfMonth);
+                ((HermesActivity)mBookCourtDateFrag.Activity).DateEsp = date.ToString("d MMMM",
+                    CultureInfo.CreateSpecificCulture("es-CL"));
+                ((HermesActivity)mBookCourtDateFrag.Activity).Date = date.ToString("yyyy-MM-dd");
+            }
+        }
+    }
 }
 
